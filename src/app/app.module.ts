55971cbdd13f6b7949ff1f41d1dd7bc1595db202
Anand Murugan <anandmurugan@MacBook-Pro.local>
2021-03-13 11:46:03 -0600

    frontend code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ZineComponent } from './zine/zine.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material/app-material.module';
+import { ValidateEqualModule } from 'ng-validate-equal';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ZineComponent
+    ZineComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    ValidateEqualModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from  '@angular/material/dialog';
+import { LoginComponent} from '../login/login.component';
+import { SignupComponent} from '../signup/signup.component';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public loginDialog: MatDialog, public signupDialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+
+  showLogin(): void {
+  	let loginDialogRef = this.loginDialog.open(LoginComponent, { width: '250px', position : {top: '60px', right: '60px'} });
+  }
+
+  showSignup(): void {
+	let signupDialogRef = this.signupDialog.open(SignupComponent, { width: '250px', position : {top: '60px', right: '60px'} });
+  }
+
 
 }
